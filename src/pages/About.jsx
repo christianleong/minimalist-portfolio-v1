e7414222e209c55575cb9f4dbbc3f70bce135e80
christianleong <christianleong@gmail.com>
2024-03-17 22:51:24 +1100

    update projects page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,11 +10,11 @@ export default function About() {
                         <p className="font-light mb-2 max-w-lg">Hi! My name is Christian, I love the internet and building things for it. My interest in development started many years ago, though it never became a part of my career until now. I have dabled in development from time to time for my own needs and out of pure interest.</p>
                         <p className="font-light mb-2 max-w-lg">I have a background in engineering where I developed a love for coding, I have also started two businesses where I developed a love for business and solving clients problems. It turns out that web development is a perfect sweet spot!</p>
                         <p className="font-light mb-2 max-w-lg">Right now, I am seeking an entry/junior role in web development, particularly in front-end where I can bring designs to life.</p>
-                        <p className="font-light mb-2 max-w-lg">Here are a few technologies I have been working with recently:</p>
+                        {/* <p className="font-light mb-2 max-w-lg">Here are a few technologies I have been working with recently:</p> */}
                     </div>
-                    <div className="potrait-img max-w-lg relative w-10/12">
+                    {/* <div className="potrait-img max-w-lg relative w-10/12">
                         <img src={"https://fakeimg.pl/500x500"} alt="" />
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,39 +1,92 @@
 import './Projects.css'
 import tictactoe from '../assets/tictactoe.png'
+import pinspiration from '../assets/pinspiration.png'
+import pipeline from '../assets/pipeline.png'
+import kickoffAnalysis from '../assets/kickoff-analysis.png'
+import { useMediaQuery } from 'react-responsive'
 
 export default function Projects() {
+
+    const isMobile = useMediaQuery({ query: '(max-width: 767px)' })
+    const isDesktopOrLaptop = useMediaQuery({ query: '(min-width: 768px)' })
+
     return (
-       <div data-anchor="projects ">
-            <div className="fp-tableCell ">
-                <div className="grid grid-rows-1 gap-5 items-center w-full">
-                    <div className="intro">
-                        <h2 className="mb-2 text-2xl font-bold">Projects</h2>
-                        <p className="font-light mb-2 max-w-lg">Here are some of the projects I worked on at General Assembly.</p>
+        <>
+        {
+            isMobile &&        
+            <div data-anchor="projects ">
+                    <div className="projects-wrapper-mobile ">
+                        <div className="grid grid-rows-1 gap-5 items-center w-full">
+                            <div className="intro">
+                                <h2 className="mb-2 text-2xl font-bold">Projects</h2>
+                                <p className="font-light mb-2 max-w-lg opacity-70">Here are some projects I worked on at General Assembly.</p>
+                            </div>
+                            <div className="projects-grid-wrapper gap-14">
+                                <div className="max-w-sm">
+                                    <img src={tictactoe} alt="" className="rounded-lg w-full h-60 mb-5"/>
+                                    <h2 className="mb-2 text-2xl font-bold">Tic-Tac-Toe</h2>
+                                    <p className='font-light mb-2 max-w-lg opacity-70'>An interactive player vs player (PvP) game showcasing proficiency in front-end web development, featuring player input handling and dynamic styling.</p>
+                                </div>
+                                <div className="max-w-sm">
+                                    <img src={pinspiration} alt="" className="rounded-lg w-full h-60 mb-5"/>
+                                    <h2 className="mb-2 text-2xl font-bold">Pinspiration</h2>
+                                    <p className='font-light mb-2 max-w-lg opacity-70'>A full-stack CRUD app that allows users to post, edit and delete their inspirations through captivating images. It also has signup and login features and the ability to allow users to search for images based on keywords in the title.</p>
+                                    <p className='font-light mb-2 max-w-lg opacity-70'>Note: It may take a moment to load.</p>
+                                </div>
+                                <div className="max-w-sm">
+                                    <img src={pipeline} alt="" className="rounded-lg w-full h-60 mb-5"/>
+                                    <h2 className="mb-2 text-2xl font-bold">Pipeline</h2>
+                                    <p className='font-light mb-2 max-w-lg opacity-70'>A full-stack single-page application that integrates with GoogleMaps API to display all petrol stations within the boundary based on current location.</p>
+                                    <p className='font-light mb-2 max-w-lg opacity-70'>A collaborative effort involving four team members utilising GitHub branches and Trello board to delegate tasks and manage workflow.</p>
+                                </div>
+                                <div className="max-w-sm mb-20">
+                                    <img src={kickoffAnalysis} alt="" className="rounded-lg w-full h-60 mb-5"/>
+                                    <h2 className="mb-2 text-2xl font-bold">Kickoff Analysis</h2>
+                                    <p className='font-light mb-2 max-w-lg opacity-70'>A single-page application built using React to display the English Premier League (EPL) football match fixtures, standings and team stats.</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="projects-grid-wrapper">
-                        <div className="max-w-sm">
-                            <img src={tictactoe} alt="" className="rounded-lg w-full"/>
-                            <h2 className="mb-2 text-2xl font-bold">Tic-Tac-Toe</h2>
-                            <p>An interactive player vs player (PvP) game showcasing proficiency in front-end web development, featuring player input handling and dynamic styling.</p>
-                        </div>
-                        <div className="max-w-sm">
-                            <img src="https://fakeimg.pl/350x233" alt="" className="rounded-lg w-full"/>
-                            <h2 className="mb-2 text-2xl font-bold">Project 2</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia eos ea aspernatur in vitae quidem enim voluptatem ducimus asperiores</p>
-                        </div>
-                        <div className="max-w-sm">
-                            <img src="https://fakeimg.pl/350x233" alt="" className="rounded-lg w-full"/>
-                            <h2 className="mb-2 text-2xl font-bold">Project 3</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia eos ea aspernatur in vitae quidem enim voluptatem ducimus asperiores</p>
-                        </div>
-                        <div className="max-w-sm">
-                            <img src="https://fakeimg.pl/350x233" alt="" className="rounded-lg w-full"/>
-                            <h2 className="mb-2 text-2xl font-bold">Project 4</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia eos ea aspernatur in vitae quidem enim voluptatem ducimus asperiores</p>
-                        </div>
-                    </div>
-                </div>
             </div>
-        </div>
+        }
+        
+        {
+            isDesktopOrLaptop &&        
+            <div data-anchor="projects ">
+                    <div className="projects-wrapper-desktop ">
+                        <div className="grid grid-rows-1 gap-5 items-center w-full">
+                            <div className="intro">
+                                <h2 className="mb-2 text-2xl font-bold">Projects</h2>
+                                <p className="font-light mb-2 max-w-lg opacity-70">Here are some projects I worked on at General Assembly.</p>
+                            </div>
+                            <div className="projects-grid-wrapper">
+                                <div className="max-w-sm">
+                                    <img src={tictactoe} alt="" className="rounded-lg w-full h-60 mb-5  mt-5"/>
+                                    <h2 className="mb-2 text-2xl font-bold">Tic-Tac-Toe</h2>
+                                    <p className='font-light mb-2 max-w-lg opacity-70'>An interactive player vs player (PvP) game showcasing proficiency in front-end web development, featuring player input handling and dynamic styling.</p>
+                                </div>
+                                <div className="max-w-sm">
+                                    <img src={pinspiration} alt="" className="rounded-lg w-full h-60 mb-5 mt-5"/>
+                                    <h2 className="mb-2 text-2xl font-bold">Pinspiration</h2>
+                                    <p className='font-light mb-2 max-w-lg opacity-70'>A full-stack CRUD app that allows users to post, edit and delete their inspirations through captivating images. It also has signup and login features and the ability to allow users to search for images based on keywords in the title.</p>
+                                    <p className='font-light mb-2 max-w-lg opacity-70'>Note: It may take a moment to load.</p>
+                                </div>
+                                <div className="max-w-sm">
+                                    <img src={pipeline} alt="" className="rounded-lg w-full h-60 mb-5 mt-5"/>
+                                    <h2 className="mb-2 text-2xl font-bold">Pipeline</h2>
+                                    <p className='font-light mb-2 max-w-lg opacity-70'>A full-stack single-page application that integrates with GoogleMaps API to display all petrol stations within the boundary based on current location.</p>
+                                    <p className='font-light mb-2 max-w-lg opacity-70'>A collaborative effort involving four team members utilising GitHub branches and Trello board to delegate tasks and manage workflow.</p>
+                                </div>
+                                <div className="max-w-sm">
+                                    <img src={kickoffAnalysis} alt="" className="rounded-lg w-full h-60 mb-5 mt-5"/>
+                                    <h2 className="mb-2 text-2xl font-bold">Kickoff Analysis</h2>
+                                    <p className='font-light mb-2 max-w-lg opacity-70'>A single-page application built using React to display the English Premier League (EPL) football match fixtures, standings and team stats.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        }
+        </>
     )
 }
