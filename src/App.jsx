78d0b09f24681e1c02eb1e5bf5ae02b39ab69581
christianleong <christianleong@gmail.com>
2024-03-25 13:13:12 +1100

    add fullpage.js feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,49 +2,69 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './pages/Hero'
 import About from './pages/About'
-import Experience from './pages/Experience'
-import Projects from './pages/Projects'
-import Contact from './pages/Contact'
 import SocialSidebar from './components/SocialSidebar'
-import New from './pages/New'
 import { useEffect, useState } from 'react';
 import { useMediaQuery } from 'react-responsive'
 import MenuOverlay from './components/MenuOverlay'
+import ReactFullpage from "@fullpage/react-fullpage";
+import Contact from './pages/Contact'
+
+const FullpageKey = import.meta.env.VITE_SOME_KEY_FULLPAGE_JS
 
 function App() {
-
   const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
   const [screenHeight, setScreenHeight] = useState(window.innerHeight);
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   useEffect(() => {
-      function handleResize() {
-          setScreenHeight(window.innerHeight)
-      }
-      window.addEventListener('resize', handleResize)
-      return () => {
-          window.removeEventListener('resize', handleResize);
-      };
+    function handleResize() {
+      setScreenHeight(window.innerHeight)
+    }
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
   },[])
 
   return (
     <>
       <div className='flex flex-col'>
-          <Navbar navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen} />
-          <MenuOverlay navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen} />
+        <Navbar navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen} />
+        <MenuOverlay navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen} />
+        { !isMobile // desktop pages
+          ? <ReactFullpage
+            debug /* Debug logging */
+            // fullpage options
+            licenseKey={FullpageKey}
+            sectionSelector='.fullpage-section'
+            credits='false'
+            navigation
+            navigationPosition='left'
+            menu='#menu'
+            scrollingSpeed={1000}
+            render={(comp) => (
+              <ReactFullpage.Wrapper>
+                <section className="fullpage-section" data-anchor="home" style={{ height: `${screenHeight}px` }}>
+                  <Hero screenHeight={screenHeight} onSectionChange={handleSectionChange}/>
+                </section>
+                <section className="fullpage-section" data-anchor="about" style={{ height: `${screenHeight}px` }}>
+                  <About screenHeight={screenHeight} onSectionChange={handleSectionChange}/>
+                </section>
+                <section className="fullpage-section" data-anchor="contact" style={{ height: `${screenHeight}px` }}>
+                  <Contact screenHeight={screenHeight} onSectionChange={handleSectionChange}/>
+                </section>
+              </ReactFullpage.Wrapper>
+            )}
+          />
+          : // mobile pages
           <main className={`fullpage-wrapper ${ isMobile ? 'p-9' : '' }`}>
-            {/* <div> */}
-              <div className='mb-20'>
-                <Hero screenHeight={screenHeight}/>
-              </div>
+              <Hero screenHeight={screenHeight}/>
               <About screenHeight={screenHeight}/>
-              {/* <Experience /> */}
-              {/* <New /> */}
-              {/* <Projects /> */}
-              {/* <Contact /> */}
+              <Contact screenHeight={screenHeight}/>
               <SocialSidebar />
-            {/* </div> */}
           </main>
+        }
+        <SocialSidebar />
       </div>
     </>
   )
