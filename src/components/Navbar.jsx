d13d2352c11c08c9f975ffdbdf687cfc843384c6
christianleong <christianleong@gmail.com>
2024-03-25 13:06:43 +1100

    add comments, remove react Scroll and replace with anchor tag for fullpage.js, refactor hamburgerBtn component, add feature to separate name into one/two lines depending on screen size, add feature to style navlink to indicate current active page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,59 +1,46 @@
 import { useMediaQuery } from "react-responsive";
-import { Button, Link } from "react-scroll";
-import NavItem from "./NavItem";
+import { Link } from "react-scroll"; // removed as there is a conflict between fullpage.js and react scroll
+import HamburgerBtn from "./HamburgerBtn";
+import './Navbar.css'
 
 const navigation = [
-  { name: "hero", page:"Home" },
-  { name: "about", page:"About" },
-  { name: "experience", page:"Experience" },
-  { name: "projects", page:"Projects" },
-  { name: "contact", page:"Contact" },
+  { name: "home", page: "Home", href: "/#home" },
+  { name: "about", page: "About", href: "/#about" },
+  { name: "experience", page: "Experience", href: "/#experience" },
+  { name: "projects", page: "Projects", href: "/#projects" },
+  { name: "contact", page: "Contact", href: "/#contact" },
 ];
 
 export default function Navbar({ navbarOpen, setNavbarOpen }) {
 
-  const isMobile = useMediaQuery({ query: '(max-width: 767px)' })
-  const isFixedToTop = isMobile ? 'relative' : 'fixed'
-  const nameVisibility = navbarOpen ? 'invisible' : 'visible'
+  const isMobile = useMediaQuery({ query: '(max-width: 767px)' }) // mobile mode breakpoint
+  const isFixedToTop = isMobile ? 'relative' : 'fixed' // navbar is fixed when in desktop mode.
+  const nameVisibility = navbarOpen ? 'invisible' : 'visible' // name on top left is hidden when the menu is open in mobile mode.
 
-  const handleClick = () => setNavbarOpen(!navbarOpen)
+  const handleClick = () => setNavbarOpen(!navbarOpen) // function passed down to HamburgerBtn so when a link is clicked, it closes the navbar in mobile mode.
 
   return (
     <nav className={`flex justify-between p-10 z-50 w-full ${navbarOpen ? 'fixed' : isFixedToTop}`}>
-        <Link to="hero">
-            <h1 className={`font-robotoSlab text-2xl font-bold flex items-center cursor-pointer ${nameVisibility}`}>
-                Christian <br />Leong
-            </h1>
-        </Link>
-        {!isMobile && (
-            <ul className="flex list-none items-center p-0 font-light">
-                {navigation.map((link, idx) => 
-                  <NavItem key={idx} to={link.name} text={link.page} />
-                )}
-            </ul>
-        )}
-        {isMobile && 
-          <button 
-          className="lg:hidden flex top-0 right-0 z-20 relative w-10 h-10 text-black focus:outline-none"
-            onClick={handleClick}>
-            <div className="absolute w-5 transform -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
-                <span
-                className={`absolute h-0.5 w-5 bg-black transform transition duration-300 ease-in-out ${
-                    navbarOpen ? "rotate-45 delay-200" : "-translate-y-1.5"
-                }`}
-                ></span>
-                <span
-                className={`absolute h-0.5 bg-black transform transition-all duration-200 ease-in-out ${
-                    navbarOpen ? "w-0 opacity-50" : "w-5 delay-200 opacity-100"
-                }`}
-                ></span>
-                <span
-                className={`absolute h-0.5 w-5 bg-black transform transition duration-300 ease-in-out ${
-                    navbarOpen ? "-rotate-45 delay-200" : "translate-y-1.5"
-                }`}
-                ></span>
-            </div>
-          </button> }
+
+      <a href={navigation[0].href}>
+          <h1 className={`font-robotoSlab text-2xl font-bold flex items-center cursor-pointer ${nameVisibility}`}>
+              Christian {isMobile ? "" : <br/>}Leong
+          </h1>
+      </a>
+
+      {/* to display navbar links in desktop mode, id="menu" and data-menuanchor are needed with fullpage.js so that class="active" is tracked. lockAnchors in fullpage.js must be set to default 'false', otherwise navbar links won't work.*/}
+      {!isMobile && (
+          <ul className="flex list-none items-center p-0" id="menu" >
+              {navigation.map((link, idx) => (
+                <li key={idx} className="m-4 font-light relative" data-menuanchor={link.name} >
+                  <a href={link.href}>{link.page}</a>
+                </li>
+              ))}
+          </ul>
+      )}
+
+      {isMobile && <HamburgerBtn onClick={handleClick} navbarOpen={navbarOpen} /> }
+
     </nav>
   )
 }
