0ce2145f0c68ed2a3c719c902ef27031f8a2d873
christianleong <christianleong@gmail.com>
2024-03-27 16:36:30 +1100

    add animate typing effect to name header on Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,43 @@
+import { useMediaQuery } from 'react-responsive';
+import Lottie from "lottie-react";
+import HeroAnimation from "../assets/hero-animation.json";
+
+export default function Home({ screenHeight }) {
+    const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
+
+    return (
+        <div 
+            id={isMobile ? 'home' : undefined}
+            className="p-0 table table-fixed w-full" 
+            style={{ 
+                marginTop: isMobile ? '50px' : "" , 
+                height: isMobile ? '' : `${screenHeight}px` 
+            }}
+        >
+
+            <div 
+                className={ isMobile ? "fp-tableCell-mobile" : "fp-tableCell" } 
+                style={{ height: isMobile? "0" :`${screenHeight}px` }}
+            >
+
+                <div className={`grid ${ isMobile ? 'grid-cols-1' : 'grid-cols-2' } gap-5 items-center w-full`}>
+
+                    <div className={`${ isMobile ? '' : 'justify-self-end' }`}>
+                        <h2 className="mb-2 text-2xl font-bold">Hello,</h2>
+                        <h2 className="mb-2 text-2xl font-bold animate-typing overflow-hidden whitespace-nowrap">My name is Christian.</h2>
+                        <p className="font-light mb-2 max-w-lg opacity-70">I am a software engineer with a background in accounting.</p>
+                        <p className="font-light mb-2 max-w-lg opacity-70">With attention to detail, I enjoy creating solutions to improve application performance and user experience.</p>
+                        <a className="cursor-pointer font-semibold underline" href="https://drive.google.com/file/d/1gHDGPrNgIq0ykIfKCDy3cU-TIetTHUNo/view?usp=sharing">View my Resume</a>
+                    </div>
+                    
+                    <div className={`max-w-lg relative w-10/12 `}>
+                        <Lottie animationData={HeroAnimation} />
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,32 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      robotoSlab: ['robotoSlab', 'serif']
+      robotoSlab: ["robotoSlab", "serif"],
     },
     extend: {},
+    keyframes: {
+      typing: {
+        "0%": {
+          width: "0%",
+          visibility: "visible",
+        },
+        "100%": {
+          width: "100%",
+        },
+      },
+      blink: {
+        "50%": {
+          borderColor: "transparent",
+        },
+        "100%": {
+          borderColor: "white",
+        },
+      },
+    },
+    animation: {
+      typing: "typing 2s steps(20) infinite alternate, blink .7s",
+      caret: "typing 2s steps(20) forwards, blink 1s steps(20) infinite 2s",
+    },
   },
   plugins: [],
 };
