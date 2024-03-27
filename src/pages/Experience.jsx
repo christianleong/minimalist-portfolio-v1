81f55a34c01e4d4f628a3816730b28912336af8b
christianleong <christianleong@gmail.com>
2024-03-27 16:51:13 +1100

    refactor code for Experience page

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,53 +1,36 @@
-import './Experience.css'
+import { useMediaQuery } from 'react-responsive'
+import TickIcon from '../components/TickIcon'
 
-export default function Experience() {
+export default function Experience({ navbarOpen }) {
+
+    const isMobile = useMediaQuery({ query: '(max-width: 767px)' })
+
+    const expContent = [
+        {title: "Software Engineering Student", workplace: "General Assembly", description: "After working in the accounting industry for 5 years, I started exploring more deeply into my new found passion for pragramming.", period: "Dec 2023 - Mar 2024"},
+        {title: "Tax Accountant", workplace: "MVA Bennett Pty Ltd", description: "Towards the end of 2021, Aberdeen Advisory merged with MVA Bennett which opened up avenues for expanding our skill set and collaborating with a fresh cohort of colleagues.", period: "Oct 2021 - Nov 2023"},
+        {title: "Tax Accountant", workplace: "Aberdeen Advisory Pty Ltd", description: "With a strong commitment to personal development, I later joined Aberdeen Advisory to expand my knowledge and problem solving skills by working with a more diverse clientele. I was also given the opportunity to liaise directly with clients which enabled me to hone my communication skills.", period: "Mar 2019 - Oct 2021"},
+        {title: "Graduate Accountant", workplace: "AscendPoint", description: "Worked in a boutique accounting firm where I first developed my skills as a tax accountant, preparing financial statements and tax returns for domestic and foreign clients.", period: "Jan 2018 - Mar 2019"},
+    ]
+
     return (
-        <div id='experience' className="flex items-center justify-center bg-white px-6 md:px-60 exp-wrapper">
+        <div 
+            id={isMobile ? 'experience' : undefined} 
+            className={`flex flex-col items-center justify-center bg-white relative my-0 mx-auto py-0 px-8 ${isMobile ? "" : "w-4/5"}`}
+        >
             <div className="space-y-6 border-l-2 border-dashed">
-                <div className="relative w-full">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="absolute -top-0.5 z-10 -ml-3.5 h-7 w-7 rounded-full text-slate-500">
-                    <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
-                </svg>
-                <div className="ml-6">
-                    <h4 className="font-bold text-slate-500">Graduate Accountant</h4>
-                    <h5 className="font-normal text-slate-500">AscendPoint</h5>
-                    <p className="mt-2 max-w-screen-sm text-sm text-gray-500">Worked in a boutique accounting firm where I first developed my skills as a tax accountant, preparing financial statements and tax returns for domestic and foreign clients.</p>
-                    <span className="mt-1 block text-sm font-semibold text-slate-500">Jan 2018 - Mar 2019</span>
-                </div>
-                </div>
-                <div className="relative w-full">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="absolute -top-0.5 z-10 -ml-3.5 h-7 w-7 rounded-full text-slate-500">
-                    <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
-                </svg>
-                <div className="ml-6">
-                    <h4 className="font-bold text-slate-500">Tax Accountant</h4>
-                    <h5 className="font-normal text-slate-500">Aberdeen Advisory Pty Ltd</h5>
-                    <p className="mt-2 max-w-screen-sm text-sm text-gray-500">With a strong commitment to personal development, I later joined Aberdeen Advisory to expand my knowledge and problem solving skills in working with a more diverse clientele. I was also given the opportunity to liaise directly with clients which enabled me to hone my communication skills.</p>
-                    <span className="mt-1 block text-sm font-semibold text-slate-500">Mar 2019 - Oct 2021</span>
-                </div>
-                </div>
-                <div className="relative w-full">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="absolute -top-0.5 z-10 -ml-3.5 h-7 w-7 rounded-full text-slate-500">
-                    <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
-                </svg>
-                <div className="ml-6">
-                    <h4 className="font-bold text-slate-500">Tax Accountant</h4>
-                    <h5 className="font-normal text-slate-500">MVA Bennett Pty Ltd</h5>
-                    <p className="mt-2 max-w-screen-sm text-sm text-gray-500">Towards the end of 2021, Aberdeen Advisory merged with MVA Bennett which opened up avenues for expanding our skill set and collaborating with a fresh cohort of colleagues.</p>
-                    <span className="mt-1 block text-sm font-semibold text-slate-500">Oct 2021 - Nov 2023</span>
-                </div>
-                </div>
-                <div className="relative w-full">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="absolute -top-0.5 z-10 -ml-3.5 h-7 w-7 rounded-full text-slate-500">
-                    <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
-                </svg>
-                <div className="ml-6">
-                    <h4 className="font-bold text-slate-500">Software Engineering Student</h4>
-                    <h5 className="font-normal text-slate-500">General Assembly</h5>
-                    <p className="mt-2 max-w-screen-sm text-sm text-gray-500">After working in the accounting industry for 5 years, I started exploring more deeply into my new found passion for pragramming.</p>
-                    <span className="mt-1 block text-sm font-semibold text-slate-500">Dec 2023 - Mar 2024</span>
-                </div>
-                </div>
+                
+                {expContent.map((job, idx) => (
+                    <div className="relative w-full" key={idx}>
+                        <TickIcon navbarOpen={navbarOpen} />
+                        <div className="ml-6">
+                            <h4 className="font-bold text-slate-500">{job.title}</h4>
+                            <h5 className="font-normal text-slate-500">{job.workplace}</h5>
+                            <p className="mt-2 max-w-screen-sm text-sm opacity-70">{job.description}</p>
+                            <span className="mt-1 block text-sm font-semibold opacity-70">{job.period}</span>
+                        </div>
+                    </div>
+                ))}
+
             </div>
         </div>
     )
